7488303fae085b4b8630a85aa12824195e16a5af
Aoengo <aoengo@outlook.com>
2022-05-13 14:53:35 +0800

    add a shell file

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+echo "Prepare the environment..."
+sudo apt-get install software-properties-common 
+sudo add-apt-repository ppa:neovim-ppa/stable 
+sudo apt update 
+
+echo "Install the neovim..."
+sudo apt install -y neovim 
+
+echo "Install the Packer..."
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+FinderPath = "~/.config"
+
+if [ ! -d "$FinderPath" ];then
+    mkdir $FinderPath && cd $FinderPath
+    echo "Create a New Finder..."
+else
+    cd ~/.config
+    echo "Finder has been exists..."
+fi
+
+git clone https://github.com/Aoengo/nvim.git
+cd nvim
+
+sudo ln -s /usr/bin/nvim /usr/local/bin/vi
+nvim
+
